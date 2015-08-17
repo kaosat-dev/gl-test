cd201053c9a51eaa081aae9d6aaacf76110c6d5f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-08-17 08:17:45 +0200

    - added text output (log/testing)

--- src/outputToFile.js
+++ src/outputToFile.js
@@ -54,11 +54,29 @@ export function outputToFile(buffer, width, height, fileName){
     //var channels = inBuf.length / 4
     for (var i = 0; i < inBuf.length; ++i) {
       png.data[i] = inBuf[i]
+
     }
 
 
     png.pack().pipe(fs.createWriteStream(fileName))
   }
 
+  function log(inBuf, width, height){
+    var channels = inBuf.length / 4
+    for (var i = 0; i < channels; ++i) {
+      var r = inBuf[i*4]
+      var g = inBuf[i*4+1]
+      var b = inBuf[i*4+2]
+      var a = inBuf[i*4+3]
+
+      console.log(r,g,b,a)
+      console.log("//")
+
+    }
+  }
+
   genOutput2(buffer, width, height)
+
+
+  
 }
