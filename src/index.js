fcf93237ff7f43d3efae89ba1e92f0b0e9a2706c
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-08-17 08:22:53 +0200

    - switched default example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 var fs    = require('fs')
 //var makeShader = require('gl-shader')
 var mat4     = require('gl-mat4')
-var normals  = require('normals')
 var glslify  = require('glslify')
 
 import {makeMesaContext} from './makeContext'
+import {checkExtensions} from './checkExtensions'
 import {outputToFile} from './outputToFile'
 
-var height = 480
-var width = 640 
+var height = 8//480
+var width = 8//640 
 let {buffer, context, gl} = makeMesaContext(width,height)
 
 function cleanup(context){
@@ -16,8 +16,9 @@ function cleanup(context){
   gl.DestroyContext(context)
 }
 
+//checkExtensions(gl)
 
-import test from './test2.5'
+import test from './test1'
 
 let {render,update} = test(gl, width, height)
 
@@ -33,4 +34,6 @@ function sequenceShots(iterations=1)
   }
 }
 
-sequenceShots(5)
+
+
+sequenceShots(3)
